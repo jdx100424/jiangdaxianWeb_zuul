7ecaadc8fb8e4ef752502936ac58ea5074488e84
江达贤 <jiangdaxian@ds.cn>
2018-11-04 12:04:02 +0800

    111

--- jiangdaxianWeb_zuul-webapp/src/main/java/com/jiangdaxian/simple/fallback/SimpleFallback.java
+++ jiangdaxianWeb_zuul-webapp/src/main/java/com/jiangdaxian/simple/fallback/SimpleFallback.java
@@ -21,22 +21,17 @@ public class SimpleFallback implements ZuulFallbackProvider {
 			public InputStream getBody() throws IOException {
 				JSONObject r = new JSONObject();
 				r.put("code", "500");
-				r.put("msg", "系统错误，请求失败");
+				r.put("msg", "system error");
 				return new ByteArrayInputStream(r.toJSONString().getBytes("UTF-8"));
 			}
 
 			@Override
 			public HttpHeaders getHeaders() {
 				HttpHeaders headers = new HttpHeaders();
-				// 和body中的内容编码一致，否则容易乱码
 				headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 				return headers;
 			}
 
-			/**
-			 * * 网关向api服务请求是失败了，但是消费者客户端向网关发起的请求是OK的， * 不应该把api的404,500等问题抛给客户端 *
-			 * 网关和api服务集群对于客户端来说是黑盒子
-			 */
 			@Override
 			public HttpStatus getStatusCode() throws IOException {
 				return HttpStatus.OK;
@@ -60,8 +55,6 @@ public class SimpleFallback implements ZuulFallbackProvider {
 
 	@Override
 	public String getRoute() {
-		// return "api-user-server";//api服务id，如果需要所有调用都支持回退，则return "*"或return null
 		return "*";
 	}
-
 }
